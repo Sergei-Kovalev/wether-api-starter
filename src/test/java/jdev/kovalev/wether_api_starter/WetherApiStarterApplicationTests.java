359c25d3e1bae5e9dc235f38e1bdf65ebdaeed2d
Sergey Kovalev <s.kovalev.jdev@gmail.com>
2025-02-23 12:17:18 +0300

    init commit

---
+++ src/test/java/jdev/kovalev/wether_api_starter/WetherApiStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package jdev.kovalev.wether_api_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WetherApiStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
